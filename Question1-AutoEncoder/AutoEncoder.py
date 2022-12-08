bcda553b0a32e2aad8453b28ac3182765b7f6a3f
Gulce62 <98024902+Gulce62@users.noreply.github.com>
2022-12-08 17:33:16 +0300

    Finalize training

--- Question1-AutoEncoder/AutoEncoder.py
+++ Question1-AutoEncoder/AutoEncoder.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def initialization(Lpre, Lpost):
-    np.random.seed(50)  # TODO look for different seed values
+    np.random.seed(0)  # TODO look for different seed values
     wo = np.sqrt(6 / (Lpre + Lpost))
     parameter = np.random.uniform(-wo, wo, size=(Lpre, Lpost))
     return parameter
@@ -20,13 +20,12 @@ def initializeWeights(inputSize, hiddenSize):
     return We
 
 
-def sigmoid(z):
+def sigmoid(z, condition):
     s = 1 / (1 + np.exp(-z))
-    return s
-
-
-def sigmoidGradient(s):
-    return s * (1-s)
+    if condition == 'forward':
+        return s
+    if condition == 'gradient':
+        return s * (1 - s)
 
 
 def forwardPass(We, data):
@@ -35,59 +34,125 @@ def forwardPass(We, data):
     W2 = We['W2']
     b2 = We['b2']
 
-    Z1 = np.dot(data.T, W1) + b1
-    A1 = sigmoid(Z1)
+    Z1 = np.dot(data, W1) + b1
+    A1 = sigmoid(Z1, 'forward')
     Z2 = np.dot(A1, W2) + b2
-    A2 = sigmoid(Z2)
+    A2 = sigmoid(Z2, 'forward')
 
-    return A2, A1
-
-
-def lossMSE(oDesired, oNetwork, m):
-    MSE = np.sum(np.square(oDesired - oNetwork)) / (2*m)
-    return MSE
-
-
-def lossTykhonovRegularization(lambdaVal, We):
-    regW1 = np.sum(np.square(We['W1']))
-    regW2 = np.sum(np.square(We['W2']))
-    TR = (lambdaVal/2) * (regW1 + regW2)
-    return TR
+    cache = {'Z1': Z1,
+             'A1': A1,
+             'Z2': Z2,
+             'A2': A2}
+    return cache
 
 
-def lossKL(beta, rho, A1):
+def MSE(oDesired, oNetwork, condition):
+    if condition == 'loss':
+        m = oDesired.shape[0]
+        lossMSE = np.sum(np.square(oDesired - oNetwork)) / (2 * m)
+        return lossMSE
+    if condition == 'gradient':
+        gradMSE = (1 / 2) * (oNetwork - oDesired)
+        return gradMSE
+
+
+def TykhonovRegularization(lambdaVal, We, condition):
+    W1 = We['W1']
+    W2 = We['W2']
+    if condition == 'loss':
+        regW1 = np.sum(np.square(W1))
+        regW2 = np.sum(np.square(W2))
+        lossTR = (lambdaVal / 2) * (regW1 + regW2)
+        return lossTR
+    if condition == 'gradient':
+        gradW1 = lambdaVal * W1
+        gradW2 = lambdaVal * W2
+        return gradW1, gradW2
+
+
+def KL(beta, rho, A1, condition):
     rho_b = A1.mean(axis=0, keepdims=True)
-    KL1 = rho * np.log(rho/rho_b)
-    KL2 = (1-rho) * np.log((1-rho)/(1-rho_b))
-    KL = beta * np.sum(KL1 + KL2)
-    return KL
+    if condition == 'loss':
+        lossKL1 = rho * np.log(rho / rho_b)
+        lossKL2 = (1 - rho) * np.log((1 - rho) / (1 - rho_b))
+        lossKL = beta * np.sum(lossKL1 + lossKL2)
+        return lossKL
+    if condition == 'gradient':
+        gradKL1 = -rho / rho_b
+        gradKL2 = (1 - rho) / (1 - rho_b)
+        gradKL = beta * (gradKL1 + gradKL2)
+        return gradKL
 
 
-def backwardPass():
-    pass
+def backwardPass(We, data, params, cache):
+    m = data.shape[0]
 
-
-def aeCost(We, data, params):
-    m = data.shape[0]  # the number of samples in the data
-
-    Lin = params['Lin']
-    Lhid = params['Lhid']
     lambdaVal = params['lambda']
     beta = params['beta']
     rho = params['rho']
 
-    A2, A1 = forwardPass(We, data)
-    MSE = lossMSE(data, A2.T, m)
-    print(MSE)
-    KL = lossKL(beta, rho, A1)
-    print(KL)
-    TR = lossTykhonovRegularization(lambdaVal, We)
-    print(TR)
-    loss = MSE + KL + TR
-    print(loss)
+    Z1 = cache['Z1']
+    A1 = cache['A1']
+    Z2 = cache['Z2']
+    A2 = cache['A2']
+
+    dTRW1, dTRW2 = TykhonovRegularization(lambdaVal, We, 'gradient')
+    dKL = KL(beta, rho, A1, 'gradient')
+
+    dZ2 = MSE(data, A2, 'gradient') * sigmoid(Z2, 'gradient')
+    dW2 = (1 / m) * (np.dot(A1.T, dZ2) + dTRW2)
+    db2 = (1 / m) * np.sum(dZ2, axis=0, keepdims=True)
+
+    dZ1 = (np.dot(dZ2, We['W2'].T) + dKL) * sigmoid(Z1, 'gradient')
+    dW1 = (1 / m) * (np.dot(data.T, dZ1) + dTRW1)
+    db1 = (1 / m) * np.sum(dZ1, axis=0, keepdims=True)
+
+    grads = {'dW1': dW1,
+             'db1': db1,
+             'dW2': dW2,
+             'db2': db2}
+    return grads
+
+
+def aeCost(We, data, params):
+    # forward pass
+    cache = forwardPass(We, data)
+
+    # calculate loss
+    lossMSE = MSE(data, cache['A2'], 'loss')
+    lossTR = TykhonovRegularization(params['lambda'], We, 'loss')
+    lossKL = KL(params['beta'], params['rho'], cache['A1'], 'loss')
+    J = lossMSE + lossTR + lossKL
+
+    # backward pass
+    J_grad = backwardPass(We, data, params, cache)
+
+    return J, J_grad
+
+
+def updateParameters(We, J_grad, learningRate):
+    We['W1'] -= learningRate * J_grad['dW1']
+    We['b1'] -= learningRate * J_grad['db1']
+    We['W2'] -= learningRate * J_grad['dW2']
+    We['b2'] -= learningRate * J_grad['db2']
+    return We
+
+
+def fit(data, parameters, epochNo, learningRate):
+    We = initializeWeights(parameters['Lin'], parameters['Lhid'])
+    J_list = []
+    for epoch in range(epochNo):
+        J, J_grad = aeCost(We, data, parameters)
+        We = updateParameters(We, J_grad, learningRate)
+        J_list.append(J)
+        print('The cost at epoch =', str(epoch), 'is:', str(J))
+    return We
+
+def predict(We, data):
+    cache = forwardPass(We, data)
+    y_pred = cache['A2']
 
 
 class AutoEncoder:
     def __init__(self):
         pass
-
--- Question1-AutoEncoder/mainQ1.py
+++ Question1-AutoEncoder/mainQ1.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import ImagePreprocessing as IP
 import AutoEncoder as AE
 
+
 def read_data(filePath):
     # Read the data with its path location
     try:
@@ -32,20 +33,19 @@ print('The maximum value of the normalized dataset is:', imageData.max())
 imPP.displayImages()
 
 shapeData = imageData.shape
-imageData = imageData.reshape(shapeData[0], shapeData[1]*shapeData[2])
+imageData = imageData.reshape(shapeData[0], shapeData[1] * shapeData[2])
 print('The shape of the flattened image data is:', imageData.shape)
 
-
 Lhid = 64
-lambdaVal = 5 * (10**-4)
-beta = 0.1
+lambdaVal = 5 * (10 ** -4)
+beta = 0.01
 rho = 0.1
 
-parameters = {'Lin': imageData.shape[0],
+parameters = {'Lin': imageData.shape[1],
               'Lhid': Lhid,
               'lambda': lambdaVal,
               'beta': beta,
               'rho': rho}
 
-We = AE.initializeWeights(parameters['Lin'], parameters['Lhid'])
-AE.aeCost(We, imageData, parameters)
+We = AE.fit(imageData, parameters, 100, 0.1)
+AE.predict(We, imageData)
