52a44198b6d7fbe2598f7adc34e9a7e2ff97a51b
Gulce62 <98024902+Gulce62@users.noreply.github.com>
2022-12-08 17:50:17 +0300

    Added data

---
+++ Question2-NLP/mainQ2.py
@@ -0,0 +1,19 @@
+import h5py
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def read_data(filePath):
+    # Read the data with its path location
+    try:
+        file = h5py.File(filePath, 'r')
+        print('File path is correct!!!')
+        return file
+    except Exception:
+        print('Invalid file path!!!')
+        sys.exit(1)
+
+file = h5py.File('Question2-NLP/data2.h5', 'r')
+keys = list(file.keys())
+print(keys)
