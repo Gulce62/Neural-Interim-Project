7ad3dfdc20c591ceee5b0384ced86bdd06b71512
Gulce62 <98024902+Gulce62@users.noreply.github.com>
2022-12-10 01:23:23 +0300

    Added forward, cost, and backward

--- Question2-NLP/NLP.py
+++ Question2-NLP/NLP.py
@@ -2,13 +2,13 @@ import numpy as np
 
 
 def gaussianInitialization(dimension):
-    np.random.seed(50)
+    np.random.seed(8)
     p = np.random.normal(loc=0, scale=0.1, size=dimension)
     return p
 
 
 def initializeWeights(layerSizes):
-    WE = gaussianInitialization((layerSizes['inputSize'], layerSizes['embedSize']))
+    WE = gaussianInitialization((layerSizes['featureSize'], layerSizes['embedSize']))
     W1 = gaussianInitialization((layerSizes['embedSize'], layerSizes['hiddenSize']))
     W2 = gaussianInitialization((layerSizes['hiddenSize'], layerSizes['outputSize']))
     b1 = gaussianInitialization((1, layerSizes['hiddenSize']))
@@ -21,7 +21,6 @@ def initializeWeights(layerSizes):
                'b2': b2}
     return weights
 
-# TODO write an activation function
 
 def sigmoid(z, condition):
     sig = 1 / (1 + np.exp(-z))
@@ -30,29 +29,91 @@ def sigmoid(z, condition):
     if condition == 'gradient':
         return sig * (1 - sig)
 
+
 # TODO look for softmax function and its derivative
 
 def softmax(z, condition):
-    if condition == 'normal':
-        exp = np.exp(z)
-        sm = exp / np.sum(exp)
+    exp = np.exp(z - np.max(z))
+    sm = exp / np.sum(exp)
+    if condition == 'forward':
         return sm
-    if condition == 'stable':
-        z = z - np.max(z, axis=-1, keepdims=True)
-        exp = np.exp(z)
-        sm = exp / np.sum(exp, axis=-1, keepdims=True)
-        return sm
-    if condition == 'log':
-        z = z - np.max(z)
-        l = np.logaddexp.reduce(z)
-        sm = np.exp(z - l)
-        return sm
-    if condition == 'log stable':
-        z = z - np.max(z)
-        exp = np.exp(z)
-        sm = np.log(exp / np.sum(z, axis=0))
-        return sm
+    if condition == 'gradient':
+        return sm * (1 - sm)
 
+
+def forwardPass(X, weights):
+    WE = weights['WE']
+    W1 = weights['W1']
+    W2 = weights['W2']
+    b1 = weights['b1']
+    b2 = weights['b2']
+
+    emb = np.dot(X, WE)
+    Z1 = np.dot(emb, W1) + b1
+    A1 = sigmoid(Z1, 'forward')
+    Z2 = np.dot(A1, W2) + b2
+    A2 = softmax(Z2, 'forward')
+
+    cache = {'emb': emb,
+             'Z1': Z1,
+             'A1': A1,
+             'Z2': Z2,
+             'A2': A2}
+    return cache
+
+
+def crossEntropy(A2, y, condition):
+    m = y.shape[0]
+    if condition == 'loss':
+        lossCE = np.sum(-y * np.log(A2)) / m
+        return lossCE
+    if condition == 'gradient':
+        gradCE = -y / A2
+        return gradCE
+
+
+def backwardPass(X, y, weights, cache):
+    m = X.shape[0]
+
+    emb = cache['emb']
+    Z1 = cache['Z1']
+    A1 = cache['A1']
+    Z2 = cache['Z2']
+    A2 = cache['A2']
+
+    dZ2 = crossEntropy(A2, y, 'gradient') * softmax(Z2, 'gradient')
+    dW2 = (1 / m) * (np.dot(A1.T, dZ2))
+    db2 = (1 / m) * np.sum(dZ2, axis=0, keepdims=True)
+
+    dZ1 = (np.dot(dZ2, weights['W2'].T)) * sigmoid(Z1, 'gradient')
+    dW1 = (1 / m) * np.dot(emb.T, dZ1)
+    db1 = (1 / m) * np.sum(dZ1, axis=0, keepdims=True)
+
+    demb = (np.dot(dZ1, weights['W1'].T))
+    dWE = (1 / m) * np.dot(X.T, demb)
+
+    grads = {'dWE': dWE,
+             'dW1': dW1,
+             'db1': db1,
+             'dW2': dW2,
+             'db2': db2}
+    return grads
+
+
+def nlpCost(X, weights, y):
+    cache = forwardPass(X, weights)
+    J = crossEntropy(cache['A2'], y, 'loss')
+    J_grad = backwardPass(X, y, weights, cache)
+    return J, J_grad
+
+def predict(X, y, weights):
+    cache = forwardPass(X, weights)
+    y_pred = cache['A2']
+    print(y_pred[0])
+    print(y[0])
+    print(np.where(y[2]==1))
+    print(y_pred[2].argsort()[-50:][::-1])
+    print(-y_pred[2].argsort()[:50])
 
 class NLP:
     def __init__(self, weights, parameters, layerSizes):
--- Question2-NLP/mainQ2.py
+++ Question2-NLP/mainQ2.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import NLP as nlp
+import OneHotEncoder as ohe
 
 
 def read_data(filePath):
@@ -26,8 +27,8 @@ X_test = file[keys[1]][()]
 y_test = file[keys[0]][()]
 words = file[keys[6]][()]
 
-print('The shape of train input is:', X_train.shape)
-print('The shape of validation input is: ', X_val.shape)
+print('\nThe shape of train input is:', X_train.shape)
+print('The shape of validation input is:', X_val.shape)
 print('The shape of test input is:', X_test.shape)
 print('The shape of train output is:', y_train.shape)
 print('The shape of validation output is:', y_val.shape)
@@ -35,37 +36,43 @@ print('The shape of test output is:', y_test.shape)
 
 print('The shape of the vocabulary is:', words.shape)
 
-batchSize = 200
-learningRate = 0.15
-momentumRate = 0.85
-epochNo = 50
+encode = ohe.OneHotEncoder(words.shape[0])
+
+X_trainOHE = encode.vectorize(X_train)
+X_trainOHE = encode.feedInput(X_trainOHE, 'concatenate')
+X_valOHE = encode.vectorize(X_val)
+X_valOHE = encode.feedInput(X_valOHE, 'concatenate')
+X_testOHE = encode.vectorize(X_test)
+X_testOHE = encode.feedInput(X_testOHE, 'concatenate')
+y_trainOHE = encode.vectorize(y_train.reshape(y_train.shape[0], 1)).squeeze()
+y_valOHE = encode.vectorize(y_val.reshape(y_val.shape[0], 1)).squeeze()
+y_testOHE = encode.vectorize(y_test.reshape(y_test.shape[0], 1)).squeeze()
+
+print('\nThe shape of one-hot encoded train input is:', X_trainOHE.shape)
+print('The shape of one-hot encoded validation input is:', X_valOHE.shape)
+print('The shape of one-hot encoded test input is:', X_testOHE.shape)
+print('The shape of one-hot encoded train output is:', y_trainOHE.shape)
+print('The shape of one-hot encoded validation output is:', y_valOHE.shape)
+print('The shape of one-hot encoded test output is:', y_testOHE.shape)
+
 D = 32
 P = 256
 # D = [32, 16, 8]
 # P = [256, 128, 64]
 
-parameters = {'batchSize': batchSize,
-              'learningRate': learningRate,
-              'momentumRate': momentumRate,
-              'epochNo': epochNo}
+parameters = {'batchSize': 200,
+              'learningRate': 0.15,
+              'momentumRate': 0.85,
+              'epochNo': 50}
 
-layerSizes = {'featureSize': X_train.shape[1],
+layerSizes = {'featureSize': X_train.shape[1] * words.shape[0],
               'inputSize': X_train.shape[0],
               'embedSize': D,
               'hiddenSize': P,
               'outputSize': words.shape[0]}
 
 weights = nlp.initializeWeights(layerSizes)
-
-
-print(X_train[0])
-
-
-
-
-
-
-
-
-
-
+J, J_grad = nlp.nlpCost(X_trainOHE, weights, y_trainOHE)
+print(J)
+#print(J_grad)
+nlp.predict(X_trainOHE, y_trainOHE, weights)
